992f6a3f26b9ed9e94d57d654f4334d2b516b0e3
Filip la Gre <tutenhamond@gmail.com>
2018-04-05 15:33:32 +0200

    Update datasets_info.pl

--- datasets_info.pl
+++ datasets_info.pl
@@ -1,5 +1,4 @@
-#!/usr/bin/perl
-
+#!/usr/local/bin/perl
 use DBI;
 no warnings;
 
@@ -14,14 +13,14 @@ my $sth = $dbh->prepare( $stmt );
 my $rv = $sth->execute();
 my @row;
 while (@row = $sth->fetchrow_array()) {
-  $stmt = qq(SELECT datetime(start, 'unixepoch') FROM `main`.@row order by start DESC LIMIT 1;);
+  $stmt = qq(SELECT datetime(start, 'unixepoch') FROM `main`.@row order by start ASC LIMIT 1;);
   my $sth = $dbh->prepare( $stmt );
   my $rv = $sth->execute();
   my @row2;
   while (@row2 = $sth->fetchrow_array()) {
     print "@row\nstart: @row2\n";
   }
-  $stmt = qq(SELECT datetime(start, 'unixepoch') FROM `main`.@row order by start ASC LIMIT 1;);
+  $stmt = qq(SELECT datetime(start, 'unixepoch') FROM `main`.@row order by start DESC LIMIT 1;);
   my $sth = $dbh->prepare( $stmt );
   my $rv = $sth->execute();
   my @row2;
@@ -31,7 +30,6 @@ while (@row = $sth->fetchrow_array()) {
   $stmt = qq(select count(*) FROM @row;);
   my $sth = $dbh->prepare( $stmt );
   my $rv = $sth->execute();
-
   while (@row2 = $sth->fetchrow_array()) {
     print "candles: @row2\n";
   }
@@ -41,30 +39,28 @@ while (@row = $sth->fetchrow_array()) {
   while (@row2 = $sth->fetchrow_array()) {
     print "avarage price: @row2\n";
   }
-  my $stmt = qq(SELECT high  FROM `main`.@row order by start ASC LIMIT 1;);
+  my $stmt = qq(SELECT min(low) FROM `main`.@row;);
   my $sth = $dbh->prepare( $stmt );
   my $rv = $sth->execute();
   while (@row2 = $sth->fetchrow_array()) {
     print "lowest price: @row2\n";
   }
-  my $stmt = qq(SELECT low  FROM `main`.@row order by start DESC LIMIT 1;);
+  my $stmt = qq(SELECT max(high)  FROM `main`.@row;);
   my $sth = $dbh->prepare( $stmt );
   my $rv = $sth->execute();
-
   while (@row2 = $sth->fetchrow_array()) {
     print "highest price: @row2\n";
   }
-  my $stmt = qq(SELECT sum(trades)  FROM `main`.@row order by start DESC LIMIT 1;);
+  my $stmt = qq(SELECT sum(trades)  FROM `main`.@row;);
   my $sth = $dbh->prepare( $stmt );
   my $rv = $sth->execute();
   while (@row2 = $sth->fetchrow_array()) {
     print "trades: @row2\n";
   }
-  my $stmt = qq(SELECT sum(volume)  FROM `main`.@row order by start DESC LIMIT 1;);
+  my $stmt = qq(SELECT sum(volume)  FROM `main`.@row;);
   my $sth = $dbh->prepare( $stmt );
   my $rv = $sth->execute();
   while (@row2 = $sth->fetchrow_array()) {
     print "volume: @row2\n\n";
   }
 }
-
